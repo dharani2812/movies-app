4035c7d12549f242e77842d4624b071f9e3ef97f
Dharani Dharan <dharanidharan28122004@gmail.com>
2025-05-25 23:14:41 +0530

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "76c1f9160464846c2deed023b56de2bb";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
